bc690201f4c2dae7d547aefc3c8581d52a1bb307
miguelwebcode <miguelwebcode@gmail.com>
2024-07-05 17:34:21 +0200

    Configure endpoint to add new book

--- index.js
+++ index.js
@@ -45,8 +45,7 @@ app.use(express.static("public"));
 
 async function getBooks() {
     const result = await db.query("SELECT * FROM books");
-    const resultRows = result.rows;
-    const booksWithFormattedDates = getBooksWithFormatDates(resultRows);
+    const booksWithFormattedDates = getBooksWithFormatDates(result.rows);
     return booksWithFormattedDates;
 }
 
@@ -76,8 +75,24 @@ app.get("/add-new-book", (req, res) => {
     res.render("add-new-book.ejs");
 });
 
-app.post("/add", (req, res) => {
-    res.redirect("/");
+app.post("/add", async (req, res) => {
+    //Get body values
+    const isbn = req.body.isbn;
+    const title = req.body.title;
+    const date_read = req.body.dateRead;
+    const rating = req.body.rating;
+    const summary = req.body.summary;
+    const notes = req.body.notes;
+    const cover_image = `${API_URL}${isbn}-${SIZE}.jpg`;
+
+    //Insert into db
+    try {
+        await db.query("INSERT INTO books (isbn, title, date_read, rating, summary, notes, cover_image) VALUES ($1, $2, $3, $4, $5, $6, $7);", 
+            [isbn, title, date_read, rating, summary, notes, cover_image]);
+        res.redirect("/");
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 app.post("/book-detail", (req, res) => {
