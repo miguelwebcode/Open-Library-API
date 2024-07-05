9ce410643f8225a913a33c8c1f2e58d5b6f58ba5
miguelwebcode <miguelwebcode@gmail.com>
2024-07-05 14:34:08 +0200

    Create booksList var, rename endpoints

--- index.js
+++ index.js
@@ -7,20 +7,47 @@ import pg from "pg";
 const app = express();
 const port = 3000;
 
+//Configure and connect to database
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "open_library",
+    password: 123456,
+    port: 5432,
+});
+db.connect
+
+let booksList = [{
+    id: 1,
+    isbn: 1234567890123,
+    date_read: '2024-07-05',
+    rating: 8,
+    summary: "summary",
+    notes: "notes"
+},
+{
+    id: 2,
+    isbn: 2234567890120,
+    date_read: '2024-07-03',
+    rating: 7,
+    summary: "summary 2",
+    notes: "notes 2"
+},]
+
 //To access request body
 app.use(bodyParser.urlencoded({ extended: true }));
 //Set where static files are
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {books: booksList});
 });
 
 app.get("/add-new-book", (req, res) => {
     res.render("add-new-book.ejs");
 });
 
-app.post("/add-new-book", (req, res) => {
+app.post("/add", (req, res) => {
     res.redirect("/");
 });
 
