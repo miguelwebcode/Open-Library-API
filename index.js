5606e7a4eaf09a480b14e100d50a43886bebc691
miguelwebcode <miguelwebcode@gmail.com>
2024-07-05 18:20:47 +0200

    Do update book endpoint

--- index.js
+++ index.js
@@ -44,25 +44,29 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 async function getBooks() {
-    const result = await db.query("SELECT * FROM books");
-    const booksWithFormattedDates = getBooksWithFormatDates(result.rows);
+    const result = await db.query("SELECT * FROM books ORDER BY id ASC");
+    const booksWithFormattedDates = formatAllBooksDates(result.rows);
     return booksWithFormattedDates;
 }
 
-function getBooksWithFormatDates(arrayOfObjects) {
+function formatAllBooksDates(arrayOfObjects) {
     const result = [];
     arrayOfObjects.forEach((book) => {
-        const date = new Date(book.date_read);
-        var year = date.getFullYear();
-        var month = (date.getMonth() + 1).toString().padStart(2, '0'); // Months are 0 based
-        var day = date.getDate().toString().padStart(2, '0');
-
-        var formattedDate = day + '-' + month + '-' + year;
-        book.date_read = formattedDate;
+        book = getBookWithFormatDate(book);
         result.push(book);
     });
-
     return result;
+}
+
+function getBookWithFormatDate(book) {
+    const date = new Date(book.date_read);
+    var year = date.getFullYear();
+    var month = (date.getMonth() + 1).toString().padStart(2, '0'); // Months are 0 based
+    var day = date.getDate().toString().padStart(2, '0');
+
+    var formattedDate = year + '-' + month + '-' + day;
+    book.date_read = formattedDate;
+    return book;
 }
 
 
@@ -84,6 +88,7 @@ app.post("/add", async (req, res) => {
     const summary = req.body.summary;
     const notes = req.body.notes;
     const cover_image = `${API_URL}${isbn}-${SIZE}.jpg`;
+    console.log(date_read)
 
     //Insert into db
     try {
@@ -98,9 +103,45 @@ app.post("/add", async (req, res) => {
 app.post("/book-detail", async (req, res) => {
     const bookId = req.body.bookId;
     const result = await db.query("SELECT * FROM books WHERE id = $1", [bookId]);
-    const book = result.rows[0];
+    let book = result.rows[0];
+    book = getBookWithFormatDate(book);
     res.render("book-detail.ejs", {book: book});
 });
+
+app.post("/edit", async (req, res) => {
+    const bookId = req.body.bookId;
+    try {
+        const result = await db.query("SELECT * FROM books WHERE id = $1", [bookId]);
+        let book = result.rows[0];
+        book = getBookWithFormatDate(book);
+        res.render("edit-book.ejs", {book: book});
+    } catch (error) {
+        console.log(error);
+    }
+    
+});
+
+app.post("/update", async (req, res) => {
+    const bookId = req.body.bookId;
+    const isbn = req.body.isbn;
+    const title = req.body.title;
+    const date_read = req.body.dateRead;
+    const rating = req.body.rating;
+    const summary = req.body.summary;
+    const notes = req.body.notes;
+    const cover_image = `${API_URL}${isbn}-${SIZE}.jpg`;
+
+    try {
+        await db.query("UPDATE books SET isbn = $2, title = $3, date_read = $4, rating = $5, summary = $6, notes = $7, cover_image = $8  WHERE id = $1", 
+            [bookId, isbn, title, date_read, rating, summary, notes, cover_image]);
+        res.redirect("/");
+    } catch (error) {
+        console.log(error);
+    }
+    
+});
+
+
 
 
 app.listen(port, () => {
